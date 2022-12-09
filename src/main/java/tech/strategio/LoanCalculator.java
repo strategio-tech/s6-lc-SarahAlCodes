41468f51eb6697366e64ccaba5497d8d85f6f191
SarahAlCodes <salsaid1016@gmail.com>
2022-12-09 13:34:42 -0500

    Feat: Added solution to loan calculator

--- src/main/java/tech/strategio/LoanCalculator.java
+++ src/main/java/tech/strategio/LoanCalculator.java
@@ -7,19 +7,25 @@ public class LoanCalculator {
     /**
      * TODO: finish out this JavaDoc comment block.
      * FIXME: WHAT DOES THIS METHOD DO?
-     * 
-     * @param ???
-     * @return ???
+     *
+     * @param is the initial number given for the loan
+     *           returns the updated number after each deduction
+     * @return the total amount remaining after deducting 10% from the three loop
      */
     static int getRemainingAmountIn3Months(int amount) {
-        // TODO: Rewrite this method
-        return -1;
+        int remainingAmount;
+        for (int i = 1; i <= 3; i++) {
+            remainingAmount = amount - (amount * 10/100);
+            amount = remainingAmount;
+        }
+        return amount;
     }
 
-    public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            int amount = scanner.nextInt();
-            System.out.println(getRemainingAmountIn3Months(amount));
+        public static void main (String[]args){
+            try (Scanner scanner = new Scanner(System.in)) {
+                int amount = scanner.nextInt();
+                System.out.println(getRemainingAmountIn3Months(amount));
+            }
         }
     }
-}
+
